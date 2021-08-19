3f5b5c4e5e15aff8382673d9dba75e9615335dc5
Nikhil-Kumaran <nikhilkumaran.dev@gmail.com>
2021-08-19 13:40:20 +0530

    📦 New: Add variables and functionality files

---
+++ functionality.js
@@ -0,0 +1,126 @@
+// Exponent Power
+// Long Hand
+Math.pow(2, 3); // 8
+Math.pow(2, 2); // 4
+Math.pow(4, 3); // 64
+
+// Short Hand
+2 ** 3; // 8
+2 ** 4; // 4
+4 ** 3; // 64
+
+// Double NOT bitwise operator (~~)
+//Longhand 
+const floor = Math.floor(6.8); // 6 
+
+// Shorthand 
+const floor = ~~6.8; // 6
+// only works for 32 bit integers (2**31) - 1
+
+// String to Number
+// Long Hand
+const num1 = parseInt('100');
+const num2 = parseFloat('100.01');
+
+// Short Hand
+const num1 = +'100'; // converts to int data type
+const num2 = +'100.01'; // converts to float data type
+
+// Deep cloning of multi-level object
+let obj = {x: 20, y: {z: 30}}; 
+
+//Longhand 
+const makeDeepClone = (obj) => { 
+  let newObject = {}; 
+  Object.keys(obj).map(key => { 
+    if(typeof obj[key] === 'object'){ 
+      newObject[key] = makeDeepClone(obj[key]); 
+    } else { 
+      newObject[key] = obj[key]; 
+    } 
+  }); 
+ return newObject; 
+} 
+const cloneObj = makeDeepClone(obj); 
+
+//Shorthand 
+const cloneObj = JSON.parse(JSON.stringify(obj));
+// doesn’t work if your object property contains function, undefined or NaN as value.
+
+// Swap Two Variables
+let x = 'Hello';
+let y = 55;
+
+//Longhand
+const temp = x;
+x = y;
+y = temp;
+
+//Shorthand
+[x, y] = [y, x];
+
+// Reverse a String - Functionality
+const stringReverse = (str) => str.split('').reverse().join('');
+const outputStringReversed = stringReverse('Welcome to the session :D');
+console.log(outputStringReversed);
+
+// Capitalize a string
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+const capitalizedOuput = capitalize('javascript one-liners are fun');
+console.log(capitalizedOuput);
+
+// Repeat n times
+//Longhand
+let str = '';
+for (let i = 0; i < 5; i++) {
+  str += 'Hello ';
+}
+console.log(str); // Hello Hello Hello Hello Hello
+
+// Shorthand
+'Hello '.repeat(5);
+
+// Min and Max
+// Longhand
+function max(arr) {
+    arr.reduce((acc, a) => a > acc ? a : acc ,arr[0]) // use < for min
+}
+
+// Shorthand 
+const arr = [2, 8, 15, 4]; 
+Math.max(...arr); // 15 
+Math.min(...arr); // 2
+
+// Generate a random id
+const randomID = Math.random().toString(16).slice(2);
+console.log(randomID);
+
+// Generate a randome hex color
+const hexColor = () => '#' + (~~(Math.random() * 0xffffff)).toString(16);
+console.log(hexColor());
+
+// Difference between two days
+const dayDif = (date1, date2) =>
+  Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 60 * 60 * 24));
+
+const numDays = dayDif(new Date('2020-09-23'), new Date('2020-10-01'));
+
+
+// Mandatory Parameter
+// Longhand
+function foo(bar) {
+    if(bar === undefined) {
+      throw new Error('Missing parameter!');
+    }
+    return bar;
+}
+  
+// Shorthand
+mandatory = () => { // Common function
+    throw new Error('Missing parameter!');
+};
+  
+foo = (bar = mandatory()) => {
+    return bar;
+};
+  
---
+++ variables.js
@@ -0,0 +1,79 @@
+// Declare Variables
+// Long Hand
+let x;
+let y;
+let z = 3;
+
+// Short Hand
+let x, y, z = 3;
+
+// Assigning Values to multiple variables
+//Longhand
+let a, b, c;
+a = 5;
+b = 8;
+c = 12;
+
+//Shorthand
+let [a, b, c] = [5, 8, 12];
+
+// Defining variables from objects
+const user = {
+    id: 339,
+    name: 'Fred',
+    age: 42,
+    education: {
+      degree: 'Masters',
+      fieldOfStudy: 'Computer Science'
+    }
+};
+// Longhand
+const id = user.id;
+const degree = user.education.degree;
+
+// Shorthand
+const {id, education: { degree, fieldOfStudy }} = user
+
+// Alias
+const {id, education: { degree, fieldOfStudy: major }} = user
+
+// default valie
+
+user = {
+    id: 339,
+    name: 'Fred',
+    age: 42,
+};
+
+const { education: { degree } } = user // TypeError: Cannot match against 'undefined' or 'null'.
+
+const { education: { degree } = {} } = user
+
+const { education: { school: {name} } = {} } = user;  // TypeError: Cannot match against 'undefined' or 'null'.
+
+// solution 1
+const { education: { school: {name} = {} } = {} } = user; 
+
+// solution 2
+const { education: { school: {name} } = { school: {} } } = user;
+
+
+// Decimal base exponents
+const lotsOfZeros = 10000000
+
+const exponent = 1e7
+
+/* 
+1e1 === 10
+1e2 === 100
+1e3 === 1000
+.
+.
+.
+2e1 === 20
+2e2 === 200
+.
+.
+2e8 === 200000000
+11e2 === 1100
+*/
